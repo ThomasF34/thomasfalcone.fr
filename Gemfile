8308c721fcb35044df007ff909c7197aaa4fa007
Thomas Falcone <thomas.falcone06@gmail.com>
2019-02-28 13:46:18 +0100

    Add(homepage)

--- Gemfile
+++ Gemfile
@@ -61,6 +61,10 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# My gems
+
+gem 'bootstrap'
+gem 'minitest-reporters' 
 
 group :development do
     gem 'capistrano',         require: false
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Thomas Falcone"
+  end 
+
   test "should get workinprogress" do
     get static_pages_workinprogress_url
     assert_response :success
+    assert_select("title", "Be patient - #{@base_title}")
   end
+
+  test "should get home" do 
+    get home_url 
+    assert_response :success
+    assert_select("title", "#{@base_title}")
+  end
+
+  test "should get root" do 
+    get root_url 
+    assert_response :success
+  end 
 
 end
